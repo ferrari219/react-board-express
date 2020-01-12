2083e91f5c945015843fa135ee0bb215d979e05e
MakeDesire <ferrari219@nate.com>
2020-01-12 17:13:05 +0900

    Update Write.js

--- frontend/src/routes/Write.js
+++ frontend/src/routes/Write.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import styled from 'styled-components';
 
 class Write extends Component {
   state = {
@@ -23,7 +24,8 @@ class Write extends Component {
   };
   render() {
     return (
-      <div>
+      <Wrap>
+        <h2>Write</h2>
         <p>
           <input
             type="text"
@@ -40,11 +42,55 @@ class Write extends Component {
             value={this.state.content}
           />
         </p>
-        <button onClick={this.postBoard}>전송하기 </button>
-        <Link to="/">목록</Link>
-      </div>
+        <Button>
+          <button onClick={this.postBoard}>전송하기 </button>
+          <Link to="/">목록</Link>
+        </Button>
+      </Wrap>
     );
   }
 }
+
+//styling
+const Wrap = styled.div`
+  padding: 20px;
+  input {
+    width: 100%;
+    height: 20px;
+    border: 1px solid #ccc;
+  }
+  textarea {
+    width: 100%;
+    height: 100px;
+    border: 1px solid #ccc;
+  }
+`;
+const Button = styled.div`
+  border-top: 1px solid #eee;
+  padding: 20px;
+
+  button {
+    float: right;
+    padding: 10px 20px;
+    border-radius: 5px;
+    text-decoration: none;
+    background: #212121;
+    color: #fff;
+    font-size: 16px;
+  }
+  a {
+    float: right;
+    padding: 10px 20px;
+    border-radius: 5px;
+    text-decoration: none;
+    background: #f2f2f2;
+    border: 1px solid #ddd;
+    color: #424242;
+    font-size: 16px;
+  }
+  & > button + a {
+    margin-right: 5px;
+  }
+`;
 
 export default Write;
